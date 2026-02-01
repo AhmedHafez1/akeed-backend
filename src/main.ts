a047a0610a3f5c8dd2ed7ab57ec9de014c4b94b1
A.Hafez <ahmed.abdelghany.hafez@gmail.com>
2026-02-01 10:14:24 +0200

    feat: Add DualAuthGuard and TokenValidatorService to CoreModule; integrate SecurityMiddleware in main bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SecurityMiddleware } from './core/middleware/security.middleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { rawBody: true });
+  const securityMiddleware = new SecurityMiddleware();
+  app.use(securityMiddleware.use.bind(securityMiddleware));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap().catch((err) => {
